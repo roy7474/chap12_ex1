434538efad262796625e1555951fdf5e6dc0d6aa
Roy Contreras Benitez <roy7474@gmail.com>
2023-09-01 19:11:00 -0400

    while loop created

---
+++ chap12_ex1.py
@@ -0,0 +1,43 @@
+''' 
+Exercise 1: 
+Change the socket program socket1.py to prompt the user for the URL so it can read any web page. 
+You can use split('/') to break the URL into its component parts so you can extract the host name 
+for the socket connect call. 
+dAdd error checking using try and except to handle the condition where 
+the user enters an improperly formatted or non-existent URL.”
+
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(),end='')
+
+mysock.close()
+'''
+
+import socket
+
+url = input('Please enter the URL of the website you would like to access: ')
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    mysock.connect((url, 80))
+    mysock.send('GET url')
+
+except:
+    print('There was a problem opening this webpage. Please try again!')
+    quit()
+
+while True:
+    data = sock.recv(512)
+    if len(data) < 1:
+        break
+    print data
+
+mysock.close()
